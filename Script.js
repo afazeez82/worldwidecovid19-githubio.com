fe8fdc62411376507e6683ba315662d8784fe54b
afazeez <afazeez82@gmail.com>
2020-08-12 21:36:35 -0500

    updated ajax code

--- Script.js
+++ Script.js
@@ -1,17 +1,54 @@
-// $.ajax({
-//     url: queryURLforcast,
-//     method: 'GET'
-// }).then(function (response) {
-//     console.log(response);
-//     var results = response.list;
-//     console.log(results.length);
 
-    //Get the latest totals across the world:
-//Request:
 var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://covid-19-data.p.rapidapi.com/totals?format=json",
+    "method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+        "x-rapidapi-key": "839906499fmsh2b527dd8c5ac97dp144417jsnd5fa3c686fd1",
+        "Retry-After": "120"
+       
+    }
+    
+}
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    
+    // var countryName = "Italy"
+    // var date = "2020-04-01"
+    // var settingsCountry = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     // "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-04-01&name=%3Crequired%3E",
+    //     "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-04-01&name="+ countryName ,
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+    //         "x-rapidapi-key": "839906499fmsh2b527dd8c5ac97dp144417jsnd5fa3c686fd1"
+    //     }
+    // }
+    
+    // $.ajax(settingsCountry).done(function (response) {
+    //     console.log(response);
+    // });
+
+
+
+});
+
+
+
+//add nesting
+
+var countryName = "Italy"
+var date = "2020-04-01"
+var settingsCountry = {
+	"async": true,
+	"crossDomain": true,
+    // "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-04-01&name=%3Crequired%3E",
+    "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-04-01&name="+ countryName ,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
@@ -19,6 +56,6 @@ var settings = {
 	}
 }
 
-$.ajax(settings).done(function (response) {
+$.ajax(settingsCountry).done(function (response) {
 	console.log(response);
-})
+});
